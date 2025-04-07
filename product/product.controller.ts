ac67e9c0b7d85cca07588d2899d67e5fced0f2e3
fabirueb <fabirueb@gmail.com>
2025-04-07 13:00:09 +0200

    test

---
+++ product/product.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from "@nestjs/common";
+import { GitService } from "./git.service";
+
+@Controller("git")
+export class ProductController {
+  constructor(private readonly gitService: GitService) {}
+
+  @Get("product")
+  async loadProduct() {
+    return "Das hat funktioniert";
+  }
+}
